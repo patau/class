ef5da7541daba17df527a4400cc2b71b27b91c48
mkfreeman <michael.k.freeman@gmail.com>
2018-01-17 13:12:21 -0800

    Add r-review

---
+++ r-review/activity-1/data-prep.R
@@ -0,0 +1,37 @@
+# Data Prep
+library(dplyr)
+
+# LOAD DATA: read in the `wa-county-data.csv` and `wa-voter-turnout.csv` files
+raw.county.data <- read.csv('./data/wa-county-data.csv', stringsAsFactors = FALSE)
+raw.voter.data <- read.csv('./data/wa-voter-turnout.csv', stringsAsFactors = FALSE)
+
+# Fix encoding error
+colnames(raw.county.data)[1] <- "County.name"
+# DATA WRANGLING: clean and join the data frames
+
+# County data: remove commas and percent signs from numeric columns
+FormatNumbers <- function(col, data) {
+  new.col <- gsub(",", "", data[,col])
+  new.col <- gsub("%", "", new.col)
+  return(new.col)
+}
+
+# Format the numeric columns
+county.data <- data.frame(lapply(colnames(raw.county.data), FormatNumbers, data=raw.county.data), stringsAsFactors = FALSE)
+colnames(county.data) <- tolower(colnames(raw.county.data))
+county.data <- county.data %>% 
+  mutate(county = gsub(" County", "", county.name)) %>% 
+  select(-county.name) %>% 
+  select(county, everything())
+
+# Voter turnout data: remove blank rows and "total" rows
+voter.data <- raw.voter.data %>% 
+  filter(county != "", 
+         county != "Total")
+
+voter.data <- data.frame(lapply(colnames(voter.data), FormatNumbers, data=voter.data), stringsAsFactors = FALSE)
+colnames(voter.data) <- tolower(colnames(raw.voter.data))
+
+# Join the data frames together, set as numeric
+all.data <- left_join(county.data, voter.data, by='county') 
+all.data[,2:ncol(all.data)] <- sapply(all.data[,2:ncol(all.data)], as.numeric)
---
+++ r-review/activity-1/exercise.R
@@ -0,0 +1,33 @@
+# Exercise-4
+
+# Set up: load `dplyr` and `ggplot2`
+library(dplyr)
+library(ggplot2)
+
+# Read, and then `source` the data-prep.R file. The `all.data` variable is now available
+
+
+# Create a column of percentage of registered voters who voted
+
+
+# Start by making a new `ggplot` with `all.data` as the data (no geometry yet)
+# What do you see?
+
+
+# Draw a scatter plot (with point geometry) with for the `all.data` set, 
+# with the `total.population` mapped to the x-position and `registerd.voters` mapped to the y-position.
+
+
+# Draw a scatter plot (with point geometry) with for the `all.data` set, 
+# with the `total.population` mapped to the x-position and `pct.voted` mapped to the y-position.
+# Color the points as "blue"
+
+
+# Create the same scatter plot as above, but put the county names where the dots are
+
+# Draw a scatter plot comparing the female percentage to the percent voted
+# Size the points by the population of the county
+
+
+# Draw a line graph comparing the total population to the percent male
+# Make it a green line
