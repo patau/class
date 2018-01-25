808779863251dae2ea72df2f64e39bd45f3ebc8e
mkfreeman <michael.k.freeman@gmail.com>
2018-01-24 17:40:40 -0800

    Add t-test activity

---
+++ t-tests/README.md
@@ -0,0 +1,6 @@
+# T-tests
+In this exercise, you'll implement a **t-test** (both manually, and with the built in R function) to assess a difference in means across groups. 
+
+Use the `birth-weights.R` file to ask the question, 
+
+> Is there an observable difference in baby weight for children born to mothers that are smokers / non-smokers?
---
+++ t-tests/birth-weights.R
@@ -0,0 +1,33 @@
+# Motivaing question:
+# Is there an observable difference in baby weight for smokers / non-smokers?
+
+# Install/library the `openintro package
+# install.packages('openintro')
+library(openintro)
+
+# You should now be able to `View` the `births` dataset
+
+
+# What are the mean birth weights for smokers/non-smokers
+
+
+# Store smokers/non-smokers in separate variables for easier access
+
+
+# Make an overlapping histogram of the birth weights of smokers/non-smokers
+
+
+# Pre-t-test conceptaul question: is this data paired?
+
+
+# Let's calculate the t value and confidence intervals manually
+
+
+# Compute CIs using t threshold
+
+
+# Implement a t-test to confirm our results
+
+
+
+
