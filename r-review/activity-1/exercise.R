ca4fa071bc6954fb194843429654b3da2bc173e8
Patricia Au <patau@uw.edu>
2018-01-25 16:04:44 -0800

    Class histogram

--- r-review/activity-1/exercise.R
+++ r-review/activity-1/exercise.R
@@ -5,19 +5,29 @@ library(dplyr)
 library(ggplot2)
 
 # Read, and then `source` the data-prep.R file. The `all.data` variable is now available
-
+source('./data-prep.R')
 
 # Create a column of percentage of registered voters who voted
-
+View(all.data)
+all.data <- all.data %>% 
+  mutate(pct.voted = total.voted / registered.voters)
+hist(all.data$pct.voted)
+plot(all.data$total.population, all.data$pct.voted)
+plot(all.data$registered.voters, all.data$pct.voted)
 
 # Start by making a new `ggplot` with `all.data` as the data (no geometry yet)
 # What do you see?
+ggplot(data = all.data) +
+  geom_point(mapping=aes(x=registered.voters, y=pct.voted, color=total.population))
 
 
 # Draw a scatter plot (with point geometry) with for the `all.data` set, 
 # with the `total.population` mapped to the x-position and `registerd.voters` mapped to the y-position.
+ggplot(data = all.data) +
+  geom_point(mapping=aes(label=county, x=median.age, y=female.percentage, size=total.population))
 
-
+ggplot(data = all.data) +
+  geom_text(mapping=aes(label=county, x=median.age, y=female.percentage, size=total.population))
 # Draw a scatter plot (with point geometry) with for the `all.data` set, 
 # with the `total.population` mapped to the x-position and `pct.voted` mapped to the y-position.
 # Color the points as "blue"
